070f4a354a6a6954899f9c224b449f2726938f79
Manlangit <manlangitv@gmail.com>
2023-02-12 11:55:57 -0500

    Updates

---
+++ precache-manifest.32404935ba208720065921831701c157.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "555a911df89eb02e92ec",
+    "url": "/github.io/adminApp/static/css/main.645a3dcb.chunk.css"
+  },
+  {
+    "revision": "555a911df89eb02e92ec",
+    "url": "/github.io/adminApp/static/js/main.555a911d.chunk.js"
+  },
+  {
+    "revision": "c5cc0a33159018721382",
+    "url": "/github.io/adminApp/static/js/1.c5cc0a33.chunk.js"
+  },
+  {
+    "revision": "1a10289dd998f01db24f",
+    "url": "/github.io/adminApp/static/js/runtime~main.1a10289d.js"
+  },
+  {
+    "revision": "dd0f1441a01e8f9fb9f92857375366b6",
+    "url": "/github.io/adminApp/index.html"
+  }
+];
